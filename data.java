35cd7c0975da9664ab1a98607dfd84dff5e4bb2f
mput <123297140+CongPut@users.noreply.github.com>
2023-01-22 17:02:50 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalprojectoop;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ACER
+ */
+public class data {
+    public static void main(){
+        ArrayList<String> Student_name = new ArrayList<String>();
+        Student_name.add("Andi Budiandi");
+        Student_name.add("Anya Melfissa");
+        Student_name.add("Chloe Larissa");
+
+        ArrayList<Integer> Student_id = new ArrayList<Integer>();
+        Student_id.add(20001);
+        Student_id.add(20002);
+        Student_id.add(20003);
+
+        ArrayList<Integer> Admin_id = new ArrayList<Integer>();
+        Admin_id.add(10001);
+        
+        ArrayList<String> Admin_name = new ArrayList<String>();
+        Admin_name.add("Budi Susanto");
+        
+        ArrayList<String> Course = new ArrayList<String>();
+        Course.add("Bahasa Indonesia");
+        Course.add("Matematika");
+        Course.add("Seni Budaya");
+
+        ArrayList<String> Course_id = new ArrayList<String>();
+        Course_id.add("Lang001");
+        Course_id.add("Math001");
+        Course_id.add("Art002");      
+    }
+}
